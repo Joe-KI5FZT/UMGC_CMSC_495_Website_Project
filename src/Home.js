c674fbdf5461688757511977dbe3e8ec4042b209
Kelly M Collins <90kcol@gmail.com>
2021-07-07 00:12:45 +0100

    Adjusted the website code so that it could work witht he newly created Service API

---
+++ src/Home.js
@@ -0,0 +1,10 @@
+import Jumbotron from "react-bootstrap/Jumbotron";
+
+export default function Home() {
+  return (
+    <Jumbotron>
+      <h1>Welcome to the Repair Service</h1>
+      <p>Please Click the Menu button to see a list of our repair services</p>
+    </Jumbotron>
+  );
+}
---
+++ src/components/Show.js
@@ -0,0 +1,56 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Spinner from "react-bootstrap/Spinner";
+import Jumbotron from "react-bootstrap/Jumbotron";
+import Button from "react-bootstrap/Button";
+import { withRouter } from "react-router-dom";
+
+function Show(props) {
+  const [data, setData] = useState({});
+  const [showLoading, setShowLoading] = useState(true);
+  const apiUrl = `https://service-api-495.herokuapp.com/service/${props.match.params.id}`;
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios(apiUrl);
+      setData(result.data);
+      setShowLoading(false);
+    };
+    fetchData();
+  }, []);
+  const addProduct = (id) => {
+    props.history.push({
+      pathname: `/add/${id}`,
+    });
+  };
+
+  return (
+    <div>
+      {showLoading && (
+        <Spinner animation="border" role="status">
+          <span className="sr-only">Loading...</span>
+        </Spinner>
+      )}
+      <Jumbotron>
+        <h1>Selected Service</h1>
+        <h2>{data.name}</h2>
+        <h1>Price</h1>
+        <h2>${data.price}</h2>
+        <p>{data.description}</p>
+        <p>
+          <Button
+            type="button"
+            variant="primary"
+            onClick={() => {
+              addProduct(data.id);
+            }}
+          >
+            Add
+          </Button>
+        </p>
+      </Jumbotron>
+    </div>
+  );
+}
+
+export default withRouter(Show);
